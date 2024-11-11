571613e0bae0d37931f1d692f47d5171b0aa9a58
Andrei-WongE <awonge01@student.bbk.ac.uk>
2024-11-11 16:26:26 +0000

    Setting-up project

---
+++ NUTS.R
@@ -0,0 +1,61 @@
+## ---------------------------
+##
+## Script name: NUTS
+##
+## Project: NUTS 1 and 2 aggregation
+##
+## Purpose of script: 
+##
+## Author: Andrei Wong Espejo
+##
+## Date Created: 2024-11-11
+##
+## Email: awonge01@student.bbk.ac.uk
+##
+## ---------------------------
+##
+## Notes: 
+##   
+##
+## ---------------------------
+
+## Program Set-up ------------
+
+options(scipen = 100, digits = 4) # Prefer non-scientific notation
+
+## Load required packages ----
+
+library("pacman")
+library("here")
+library("groundhog")
+
+set.groundhog.folder(here("groundhog_library"))
+groundhog.day = "2024-04-25" #"2020-05-12"
+#Dowloaded fromn https://github.com/CredibilityLab/groundhog
+
+pkgs = c("dplyr", "tidyverse", "janitor", "sf"
+         , "ggplot2","xfun", "remotes", "sp", "spdep"
+         , "foreach", "doParallel", "parallel", "progress"
+         , "doSNOW", "purrr", "patchwork"
+         , "haven", "openxlsx", "MASS", "reticulate"
+         , "future", "furrr", "data.table","leaflet"
+         , "jtools", "tidyr", "ggspatial", "raster"
+         , "prettymapr", "viridis", "labelled"
+         , "writexl", "WDI", "wesanderson", "ggrepel",
+         "ggbreak", "nuts"
+)
+
+groundhog.library(pkgs, groundhog.day
+                  , ignore.deps =  "fs")
+
+
+## Runs the following --------
+
+#https://github.com/antaldaniel/eurostat_regional/blob/master/Rearrange-Regional-Data.md
+# Nuts R package: https://cran.r-project.org/web/packages/nuts/vignettes/nuts.html
+
+## Loading data --------
+
+
+
+
---
+++ WB.R
@@ -0,0 +1,50 @@
+## ---------------------------
+##
+## Script name: WB
+##
+## Project:
+##
+## Purpose of script: 
+##
+## Author: Andrei Wong Espejo
+##
+## Date Created: 2024-11-11
+##
+## Email: awonge01@student.bbk.ac.uk
+##
+## ---------------------------
+##
+## Notes: 
+##   
+##
+## ---------------------------
+
+## Program Set-up ------------
+
+options(scipen = 100, digits = 4) # Prefer non-scientific notation
+
+## Load required packages ----
+
+library("here")
+library("groundhog")
+
+set.groundhog.folder(here("groundhog_library"))
+groundhog.day = "2024-04-25"
+#Dowloaded fromn https://github.com/CredibilityLab/groundhog
+
+pkgs = c("dplyr", "tidyverse", "janitor", "sf"
+         , "ggplot2","xfun", "remotes", "sp", "spdep"
+         , "foreach", "doParallel", "parallel", "progress"
+         , "doSNOW", "purrr", "patchwork"
+         , "haven", "openxlsx", "MASS", "reticulate"
+         , "future", "furrr", "data.table","leaflet"
+         , "jtools", "tidyr", "ggspatial", "raster"
+         , "prettymapr", "viridis", "labelled"
+         , "writexl", "WDI", "wesanderson", "ggrepel",
+         "ggbreak"
+)
+
+groundhog.library(pkgs, groundhog.day)
+
+
+## Runs the following --------
